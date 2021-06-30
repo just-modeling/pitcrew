d9dd943f78a43754e1a03bd825cbfbd60b850243
ziang jia <zjia@ksmconsulting.com>
2021-06-29 20:06:06 -0400

    update

--- k8s-hub-novnc-desktop/rootfs/startup.sh
+++ k8s-hub-novnc-desktop/rootfs/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "welcome..."
+
 if [ -n "$VNC_PASSWORD" ]; then
     echo -n "$VNC_PASSWORD" > /.password1
     x11vnc -storepasswd $(cat /.password1) /.password2
@@ -13,34 +13,38 @@ if [ -n "$X11VNC_ARGS" ]; then
 fi
 
 if [ -n "$OPENBOX_ARGS" ]; then
-    sed -i "s#^command=/usr/bin/openbox.*#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#^command=/usr/bin/openbox\$#& ${OPENBOX_ARGS}#" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 if [ -n "$RESOLUTION" ]; then
     sed -i "s/1024x768/$RESOLUTION/" /usr/local/bin/xvfb.sh
 fi
 
-echo "assign user..."
 USER=${USER:-root}
-HOME=$HOME
-echo $USER
-# if [ "$USER" != "root" ]; then
-#     echo "* enable custom user: $USER"
-#     useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
-#     if [ -z "$PASSWORD" ]; then
-#         echo "  set default password to \"ubuntu\""
-#         PASSWORD=ubuntu
-#     fi
-#     HOME=/home/$USER
-#     echo "$USER:$PASSWORD" | chpasswd
-#     cp -r /root/{.gtkrc-2.0,.asoundrc} ${HOME}
-#     [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
-# fi
-echo "dynamically modify supervisord.conf"
+HOME=/root
+if [ "$USER" != "root" ]; then
+    echo "* enable custom user: $USER"
+    useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER
+    if [ -z "$PASSWORD" ]; then
+        echo "  set default password to \"ubuntu\""
+        PASSWORD=ubuntu
+    fi
+    HOME=/home/$USER
+    echo "$USER:$PASSWORD" | chpasswd
+    cp -r /root/{.config,.gtkrc-2.0,.asoundrc} ${HOME}
+    chown -R $USER:$USER ${HOME}
+    [ -d "/dev/snd" ] && chgrp -R adm /dev/snd
+fi
 sed -i -e "s|%USER%|$USER|" -e "s|%HOME%|$HOME|" /etc/supervisor/conf.d/supervisord.conf
 
+# home folder
+if [ ! -x "$HOME/.config/pcmanfm/LXDE/" ]; then
+    mkdir -p $HOME/.config/pcmanfm/LXDE/
+    ln -sf /usr/local/share/doro-lxde-wallpapers/desktop-items-0.conf $HOME/.config/pcmanfm/LXDE/
+    chown -R $USER:$USER $HOME
+fi
+
 # nginx workers
-echo "dynamically modify nginx.conf"
 sed -i 's|worker_processes .*|worker_processes 1;|' /etc/nginx/nginx.conf
 
 # nginx ssl
@@ -57,8 +61,15 @@ if [ -n "$HTTP_PASSWORD" ]; then
 	sed -i 's|#_HTTP_PASSWORD_#||' /etc/nginx/sites-enabled/default
 fi
 
+# dynamic prefix path renaming
+if [ -n "$RELATIVE_URL_ROOT" ]; then
+    echo "* enable RELATIVE_URL_ROOT: $RELATIVE_URL_ROOT"
+	sed -i 's|#_RELATIVE_URL_ROOT_||' /etc/nginx/sites-enabled/default
+	sed -i 's|_RELATIVE_URL_ROOT_|'$RELATIVE_URL_ROOT'|' /etc/nginx/sites-enabled/default
+fi
+
 # clearup
 PASSWORD=
 HTTP_PASSWORD=
 
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+exec /bin/tini -- supervisord -n -c /etc/supervisor/supervisord.conf
